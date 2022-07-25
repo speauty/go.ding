3507385701da233f03c839819ef50aa60f8e8711
speauty <speauty@163.com>
2022-07-25 16:07:41 +0800

    新增DRMsgActionCard数据结构;

--- dto.go
+++ dto.go
@@ -31,3 +31,26 @@ type DRMsgMarkdown struct {
 	AtUserIds []string `json:"atUserIds"` // optional
 	IsAtAll   bool     `json:"isAtAll"`   // optional
 }
+
+// {
+//    "actionCard": {
+//        "title": "乔布斯 20 年前想打造一间苹果咖啡厅，而它正是 Apple Store 的前身",
+//        "text": "![screenshot](https://gw.alicdn.com/tfs/TB1ut3xxbsrBKNjSZFpXXcXhFXa-846-786.png)
+// ### 乔布斯 20 年前想打造的苹果咖啡厅
+// Apple Store 的设计正从原来满满的科技感走向生活化，而其生活化的走向其实可以追溯到 20 年前苹果一个建立咖啡馆的计划",
+//        "btnOrientation": "0",
+//        "singleTitle" : "阅读全文",
+//        "singleURL" : "https://www.dingtalk.com/"
+//    },
+//    "msgtype": "actionCard"
+//}
+
+type DRMsgActionCard struct {
+	MsgType    string `json:"msgtype"` // fixed: actionCard
+	ActionCard struct {
+		Title          string `json:"title"`
+		Text           string `json:"text"`
+		BtnOrientation uint8  `json:"btnOrientation"`
+		SingleTitle    string `json:"singleTitle"`
+	} `json:"actionCard"`
+}
